42b1e254d041068636197fabb4ed2d67ef08dd8a
Ivan <ivnsimakhin@gmail.com>
2021-01-05 18:35:56 +0300

    Updated titles loading

--- src/main/java/lab/Application.java
+++ src/main/java/lab/Application.java
@@ -2,75 +2,110 @@ package lab;
 
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
-import lab.domain.page.Page;
+import io.vavr.collection.HashSet;
+import io.vavr.collection.List;
+import io.vavr.collection.Set;
+import io.vavr.control.Option;
 import lab.services.categories.CategoriesDownloadService;
 
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.Objects;
 import java.util.Scanner;
-import java.util.concurrent.ForkJoinPool;
-import java.util.function.Function;
 
 public class Application {
     public static void main(String[] args) throws Exception {
-        test2();
+//        final String outputPath = "/home/ivan/projects/inilak/categories.txt";
+//        downloadAllCategories(outputPath);
         // ignore
-        Scanner sc = new Scanner(System.in);
-        sc.next();
+        String BASE = "/home/ivan/projects/inilak";
+
+        String categoriesPath = String.format("%s/categories.txt", BASE);
+        String titlesPath = String.format("%s/titles.txt", BASE);
+        String parsedPath = String.format("%s/parsed.txt", BASE);
+        String metaPath = String.format("%s/meta.txt", BASE);
+
+//        Disposable allItems = downloadAllItems(categoriesPath, parsedPath, metaPath);
+//        Disposable d = downloadAllTitles(categoriesPath, titlesPath);
+//        Scanner sc = new Scanner(System.in);
+//        sc.nextLine();
+//        d.dispose();
     }
 
-    private static void test1() throws Exception {
-        List<String> categories = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\ivan\\IdeaProjects\\kalinin\\kekus.txt"));
+//    private static Disposable downloadAllItems(String categoriesPath, String parsedOutput, String metaOutputPath) throws Exception {
+//
+//        PagesLoader pagesLoader = new PagesLoader();
+//
+//        Function<Page, Page> formatter = (page) -> {
+//            String text = page
+//                    .getExtract()
+//                    .replaceAll("[#*]", "")
+//                    .replaceAll("\n", " ");
+//            page.setExtract(text);
+//            return page;
+//        };
+//
+//        PageWriter pageWriter = new PageWriter(parsedOutput, metaOutputPath);
+//
+//        @NonNull Disposable disposable =
+//                Flowable.fromIterable(categories)
+//                        .take(1)
+//                        .filter(x -> x.replaceAll(":", "").length() == x.length() - 1)
+//                        .doAfterNext(System.out::println)
+//                        .parallel(8)
+//                        .runOn(Schedulers.from(ForkJoinPool.commonPool()))
+//                        .flatMap(titleLoaders::loadTitles)
+//                        .flatMap(pagesLoader::loadTexts)
+//                        .map(formatter::apply)
+//                        .sequential()
+//                        .distinct()
+//                        .subscribe(x -> {
+//                            pageWriter.write(x);
+//                            pageWriter.writeMetadata(x);
+//                        }, Throwable::printStackTrace, pageWriter::close);
+//        return disposable;
+//    }
+
+    private static Disposable downloadAllTitles(String categoriesPath, String titlesOutput) throws Exception {
+        List<String> categories = List.empty();
+        BufferedReader reader = new BufferedReader(new FileReader(categoriesPath));
         String line;
         while ((line = reader.readLine()) != null) {
-            categories.add(line);
+            categories = categories.prepend(line);
         }
+        reader.close();
 
         TitleLoaders titleLoaders = new TitleLoaders();
-        PagesLoader pagesLoader = new PagesLoader();
-
-        Function<Page, Page> formatter = (page) -> {
-            String text = page
-                    .getExtract()
-                    .replaceAll("[#*]", "")
-                    .replaceAll("\n", " ");
-            page.setExtract(text);
-            return page;
-        };
-
-        PageWriter pageWriter = new PageWriter(
-                "C:\\Users\\ivan\\IdeaProjects\\kalinin\\parsed.txt",
-                "C:\\Users\\ivan\\IdeaProjects\\kalinin\\parsed-meta.txt"
-        );
-        @NonNull Disposable disposable =
-                Flowable.fromIterable(categories)
-                        .filter(x -> x.replaceAll(":", "").length() == x.length() - 1)
-                        .doAfterNext(System.out::println)
-                        .parallel(50)
-                        .runOn(Schedulers.from(ForkJoinPool.commonPool()))
-                        .flatMap(titleLoaders::loadTitles)
-                        .flatMap(pagesLoader::loadTexts)
-                        .map(formatter::apply)
-                        .sequential()
-                        .distinct()
-                        .subscribe(x -> {
-                            pageWriter.write(x);
-                            pageWriter.writeMetadata(x);
-                        }, Throwable::printStackTrace, pageWriter::close);
-
+        Observable<String> categories$ = Observable.fromIterable(categories);
+        System.setOut(new PrintStream(new FileOutputStream(titlesOutput)));
+        return categories$
+                .flatMap(titleLoaders::loadTitles2)
+                .subscribe(System.out::println, Throwable::printStackTrace);
     }
 
-    private static void test2() throws Exception {
-        System.setOut(new PrintStream(new FileOutputStream("C:\\Users\\ivan\\IdeaProjects\\kalinin\\kekus.txt")));
+    private static void downloadAllCategories(@NonNull String outputPath) throws Exception {
+        System.setOut(new PrintStream(new FileOutputStream(outputPath)));
         CategoriesDownloadService service = new CategoriesDownloadService();
         Disposable subscribe = service.getAllCategories("Category:Cinema_of_the_United_States")
                 .subscribe(System.out::println, Throwable::printStackTrace, () -> System.err.println("Bye"));
+    }
+
+    private static void filterDuplicates(String inPath, String outPath) throws Exception {
+        Set<String> linesSet = HashSet.empty();
+        BufferedReader reader = new BufferedReader(new FileReader(inPath));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            linesSet = linesSet.add(line);
+        }
+        reader.close();
+        System.out.println("HASH SET SIZE: " + linesSet.size());
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outPath));
+        for (String s : linesSet) {
+            writer.write(s);
+            writer.write('\n');
+        }
+        writer.close();
     }
 }
--- src/main/java/lab/TitleLoaders.java
+++ src/main/java/lab/TitleLoaders.java
@@ -1,67 +1,81 @@
 package lab;
 
-import io.reactivex.rxjava3.core.BackpressureStrategy;
-import io.reactivex.rxjava3.core.Flowable;
-import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.*;
+import io.vavr.collection.List;
+import io.vavr.control.Option;
+import io.vavr.control.Try;
+import lab.domain.title.TitleContinue;
 import lab.domain.title.TitlePage;
+import lab.domain.title.TitleQuery;
 import lab.domain.title.TitleResult;
 import lab.wikiapi.WikiTitlesService;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 
 public class TitleLoaders {
+    private final static OkHttpClient client = new OkHttpClient.Builder()
+            .connectionPool(new ConnectionPool(100, 1, TimeUnit.SECONDS))
+            .build();
+
     private final static Retrofit retrofit = new Retrofit
             .Builder()
             .baseUrl("https://en.wikipedia.org")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build();
     private final static WikiTitlesService service = retrofit.create(WikiTitlesService.class);
 
-    Flowable<String> loadTitles(String category) throws Exception {
-        try {
-            TitleResult result = service
-                    .getTitles(category, null)
-                    .execute()
-                    .body();
+    public Observable<String> loadTitles2(String category) {
+        return Observable.create(obs -> this.loadAllTitles(obs, category));
+    }
 
-            List<String> titles =
-                    result.getQuery()
-                            .getCategorymembers()
-                            .stream()
-                            .map(TitlePage::getTitle)
-                            .filter(x -> !x.contains(":"))
-                            .collect(Collectors.toList());
 
-            if (result.getContinueOptional() == null) {
-                return Observable.fromIterable(titles).toFlowable(BackpressureStrategy.BUFFER);
-            }
+    private void loadAllTitles(ObservableEmitter<String> obs, String category) {
+        Try<TitleResult> getTitlesTry = this.retrieveTitles(category, Option.none());
 
-            String next = result.getContinueOptional().getCmcontinue();
-            boolean goon = true;
-            while (goon) {
-                TitleResult next_result = service.getTitles(category, next).execute().body();
-                if (next_result.getContinueOptional() == null) {
-                    goon = false;
-                } else {
-                    next = next_result.getContinueOptional().getCmcontinue();
-                }
-                titles.addAll(
-                        next_result
-                                .getQuery()
-                                .getCategorymembers()
-                                .stream()
-                                .map(TitlePage::getTitle)
-                                .filter(x -> !x.contains(":"))
-                                .collect(Collectors.toList())
-                );
-            }
+        TitleResult titleResult = getTitlesTry.get();
+        Option<String> optionNext = Option.of(titleResult.getContinueOptional())
+                .map(TitleContinue::getCmcontinue);
+        this.extractTitles(titleResult).forEach(obs::onNext);
 
-            return Observable.fromIterable(titles).toFlowable(BackpressureStrategy.BUFFER);
-        } catch (Exception exception) {
-            return this.loadTitles(category);
+        while (optionNext.isDefined()) {
+            Try<TitleResult> nextResultsTry = this.retrieveTitles(category, optionNext);
+
+            this.extractTitles(nextResultsTry.get()).forEach(obs::onNext);
+
+            optionNext = Option.of(nextResultsTry)
+                    .map(Try::get).filter(Objects::nonNull)
+                    .map(TitleResult::getContinueOptional).filter(Objects::nonNull)
+                    .map(TitleContinue::getCmcontinue).filter(Objects::nonNull);
         }
+    }
+
+    private List<String> extractTitles(TitleResult result) {
+        Option<List<String>> optionalTitles = Option.of(result)
+                .map(TitleResult::getQuery)
+                .map(TitleQuery::getCategorymembers)
+                .map(List::ofAll)
+                .map(list -> list.map(TitlePage::getTitle));
+
+        if (optionalTitles.isEmpty()) {
+            return List.empty();
+        }
+
+        Predicate<String> isNotCategory = x -> !x.contains("Category:");
+        return optionalTitles.get().filter(isNotCategory);
+    }
+
+    private Try<TitleResult> retrieveTitles(String category, Option<String> next) {
+        return Try.of(() -> service.getTitles(category, next.getOrNull()).execute().body())
+                .recover(x -> {
+                    System.err.println("[TITLE] Sad but wiki fucked up :(");
+                    return this.retrieveTitles(category, next).get();
+                });
     }
 }
